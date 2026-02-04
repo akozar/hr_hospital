c5cd6dbcc46ffb1b5ab169a55e035ad593acb5d1
alexandr_k <alexandr_k@itipteam.com>
2026-02-04 13:22:14 +0200

    HR Hospital manifest init

---
+++ __manifest__.py
@@ -0,0 +1,29 @@
+{
+    'name': 'HR Hospital',
+    'summary': '',
+    'author': 'Oleksandr Kozar',
+    'website': 'https://odoo.school/',
+    'category': 'Customizations',
+    'license': 'OPL-1',
+    'version': '19.0.0.0.0',
+
+    'depends': [
+        'base',
+    ],
+
+    'external_dependencies': {
+        'python': [],
+    },
+
+    'data': [
+    ],
+    'demo': [
+    ],
+
+    'installable': True,
+    'auto_install': False,
+
+    'images': [
+    ],
+
+}
